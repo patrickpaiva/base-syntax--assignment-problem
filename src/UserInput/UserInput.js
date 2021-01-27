9acc42f8f3969fb8c3a44a59a988648e334812df
Patrick Paiva <patrickpaiva88@gmail.com>
2021-01-27 09:45:20 -0300

    added both inline styles and stylesheets

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const UserInput = (props) => {
+    const style = {
+        color: 'green',
+        backgroundColor: 'black'
+    }
     return (
-        <input type="text" onChange={props.changed} value={props.name} />
+        <input 
+            type="text" 
+            onChange={props.changed} 
+            value={props.name}
+            style={style} 
+        />
     )
 }
 
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './UserOutput.css'
 
 const UserOutput = (props) => {
     return (
-        <div>
+        <div className="output">
             <p>Username: {props.name}</p>
             <p>Welcome</p>
         </div>
