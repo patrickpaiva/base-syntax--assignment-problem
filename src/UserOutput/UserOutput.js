d4bb8580dd0e1d6412fdd72adfb489b1b4470fcf
Patrick Paiva <patrickpaiva88@gmail.com>
2021-01-27 09:40:53 -0300

    components build and state created

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserOutput = (props) => {
+    return (
+        <div>
+            <p>Username: {props.name}</p>
+            <p>Welcome</p>
+        </div>
+    )
+}
+
+export default UserOutput
